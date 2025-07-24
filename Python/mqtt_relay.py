723644addfd1b58e461dbabea5d7005a4e7b2613
François <courtemanche.francois@gmail.com>
2025-07-24 14:37:23 -0400

    Trigger

--- Python/mqtt_relay.py
+++ Python/mqtt_relay.py
@@ -27,7 +27,7 @@ logging.basicConfig(
 )
 # -----------------------------------
 
-print("Starting Smart Plug Controller - Version 1.2")
+print("Starting Smart Plug Controller - Version 1.3")
 
 # ===== CONFIGURATION =====
 # All credentials in one place
